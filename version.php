1aebd492156805489318d2b89dc56c3c72713fd6
Mark Nelson <mark@moodle.com.au>
2012-08-15 19:00:40 +0800

    Code tidy up

--- version.php
+++ version.php
@@ -3,11 +3,11 @@
 /**
  * Code fragment to define the version of the certificate module
  *
- * @package mod
- * @subpackage  certificate
- * @copyright   Mark Nelson <mark@moodle.com.au>
- * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or late
- **/
+ * @package    mod
+ * @subpackage certificate
+ * @copyright  Mark Nelson <mark@moodle.com.au>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or late
+ */
 
 $module->version  = 2012080801;  // The current module version (Date: YYYYMMDDXX)
 $module->requires = 2010112400;  // Requires this Moodle version
